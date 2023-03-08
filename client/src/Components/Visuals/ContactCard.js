cc8dee1e53554298e3664fc191b9a224a6a02226
tymatson109@gmail.com <80430068+tymatson109@users.noreply.github.com>
2023-03-08 11:52:53 -0800

    Worked on contact me page

--- client/src/Components/Visuals/ContactCard.js
+++ client/src/Components/Visuals/ContactCard.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './ContactCard.css';
+import emailjs from '@emailjs/browser';
 
 export const ContactCard = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [error, setError] = useState('');
+    const form = useRef();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,6 +23,12 @@ export const ContactCard = () => {
             setError('Please enter a message!');
             return;
         }
+        emailjs.sendForm('service_32pwjhl', 'template_3fzno9d', form.current, 'cRqQ9TqCxKatFK9te')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
         setName('')
         setEmail('')
         setMessage('');
@@ -31,20 +39,23 @@ export const ContactCard = () => {
         <div className='contact-card'>
             <h1>Contact</h1>
             <p>Ask a question or get in touch with me?</p>
-            <form onSubmit={handleSubmit} className='contact-card-form'>
+            <form ref={form} onSubmit={handleSubmit} className='contact-card-form'>
                 <input 
+                    name={'user_name'}
                     className='contact-card-input' 
                     placeholder='Name...'
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                 />
                 <input 
+                    name={'user_email'}
                     className='contact-card-input' 
                     placeholder='Email...'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <textarea 
+                    name={'message'}
                     className='contact-card-textarea' 
                     rows={10}
                     placeholder='Message...'
